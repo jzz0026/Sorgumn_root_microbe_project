b7d8daa35dab731c80f1db38b2ba17fbc26a544d
Jinglie Zhou <jzz0026@lbl.gov>
2018-04-18 14:20:02 -0700

    new

---
+++ Sorgum_root_meta/IMG_psedo_ref_mapping_sorgum_root_meta/run_get_head.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+#$ -cwd
+#$ -l ram.c=7g 
+#$ -pe pe_slots 32 
+#$ -M superjohnlove@gmail.com 
+#$ -m abe
+#$ -l h_rt=12:00:00
+cat 2565956580_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2565956580_Sorghum_microbiome_072115-103_1_header.txt
+cat 2565956580_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2565956580_Sorghum_microbiome_072115-104_1_header.txt
+cat 2565956580_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2565956580_Sorghum_microbiome_072115-113_1_header.txt
+cat 2565956580_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2565956580_Sorghum_microbiome_072115-129_1_header.txt
+cat 2565956580_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2565956580_Sorghum_microbiome_072115-187_1_header.txt
+cat 2565956580_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2565956580_Sorghum_microbiome_072115-40_1_header.txt
+cat 2619619019_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2619619019_Sorghum_microbiome_072115-103_1_header.txt
+cat 2619619019_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2619619019_Sorghum_microbiome_072115-104_1_header.txt
+cat 2619619019_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2619619019_Sorghum_microbiome_072115-113_1_header.txt
+cat 2619619019_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2619619019_Sorghum_microbiome_072115-129_1_header.txt
+cat 2619619019_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2619619019_Sorghum_microbiome_072115-187_1_header.txt
+cat 2619619019_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2619619019_Sorghum_microbiome_072115-40_1_header.txt
+cat 2636416004_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416004_Sorghum_microbiome_072115-103_1_header.txt
+cat 2636416004_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416004_Sorghum_microbiome_072115-104_1_header.txt
+cat 2636416004_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416004_Sorghum_microbiome_072115-113_1_header.txt
+cat 2636416004_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416004_Sorghum_microbiome_072115-129_1_header.txt
+cat 2636416004_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416004_Sorghum_microbiome_072115-187_1_header.txt
+cat 2636416004_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416004_Sorghum_microbiome_072115-40_1_header.txt
+cat 2636416065_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416065_Sorghum_microbiome_072115-103_1_header.txt
+cat 2636416065_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416065_Sorghum_microbiome_072115-104_1_header.txt
+cat 2636416065_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416065_Sorghum_microbiome_072115-113_1_header.txt
+cat 2636416065_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416065_Sorghum_microbiome_072115-129_1_header.txt
+cat 2636416065_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416065_Sorghum_microbiome_072115-187_1_header.txt
+cat 2636416065_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416065_Sorghum_microbiome_072115-40_1_header.txt
+cat 2636416188_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416188_Sorghum_microbiome_072115-103_1_header.txt
+cat 2636416188_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416188_Sorghum_microbiome_072115-104_1_header.txt
+cat 2636416188_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416188_Sorghum_microbiome_072115-113_1_header.txt
+cat 2636416188_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416188_Sorghum_microbiome_072115-129_1_header.txt
+cat 2636416188_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416188_Sorghum_microbiome_072115-187_1_header.txt
+cat 2636416188_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2636416188_Sorghum_microbiome_072115-40_1_header.txt
+cat 2639762506_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762506_Sorghum_microbiome_072115-103_1_header.txt
+cat 2639762506_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762506_Sorghum_microbiome_072115-104_1_header.txt
+cat 2639762506_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762506_Sorghum_microbiome_072115-113_1_header.txt
+cat 2639762506_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762506_Sorghum_microbiome_072115-129_1_header.txt
+cat 2639762506_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762506_Sorghum_microbiome_072115-187_1_header.txt
+cat 2639762506_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762506_Sorghum_microbiome_072115-40_1_header.txt
+cat 2639762618_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762618_Sorghum_microbiome_072115-103_1_header.txt
+cat 2639762618_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762618_Sorghum_microbiome_072115-104_1_header.txt
+cat 2639762618_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762618_Sorghum_microbiome_072115-113_1_header.txt
+cat 2639762618_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762618_Sorghum_microbiome_072115-129_1_header.txt
+cat 2639762618_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762618_Sorghum_microbiome_072115-187_1_header.txt
+cat 2639762618_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2639762618_Sorghum_microbiome_072115-40_1_header.txt
+cat 2654587828_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2654587828_Sorghum_microbiome_072115-103_1_header.txt
+cat 2654587828_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2654587828_Sorghum_microbiome_072115-104_1_header.txt
+cat 2654587828_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2654587828_Sorghum_microbiome_072115-113_1_header.txt
+cat 2654587828_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2654587828_Sorghum_microbiome_072115-129_1_header.txt
+cat 2654587828_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2654587828_Sorghum_microbiome_072115-187_1_header.txt
+cat 2654587828_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2654587828_Sorghum_microbiome_072115-40_1_header.txt
+cat 2663762789_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2663762789_Sorghum_microbiome_072115-103_1_header.txt
+cat 2663762789_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2663762789_Sorghum_microbiome_072115-104_1_header.txt
+cat 2663762789_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2663762789_Sorghum_microbiome_072115-113_1_header.txt
+cat 2663762789_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2663762789_Sorghum_microbiome_072115-129_1_header.txt
+cat 2663762789_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2663762789_Sorghum_microbiome_072115-187_1_header.txt
+cat 2663762789_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2663762789_Sorghum_microbiome_072115-40_1_header.txt
+cat 2671181093_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2671181093_Sorghum_microbiome_072115-103_1_header.txt
+cat 2671181093_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2671181093_Sorghum_microbiome_072115-104_1_header.txt
+cat 2671181093_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2671181093_Sorghum_microbiome_072115-113_1_header.txt
+cat 2671181093_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2671181093_Sorghum_microbiome_072115-129_1_header.txt
+cat 2671181093_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2671181093_Sorghum_microbiome_072115-187_1_header.txt
+cat 2671181093_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2671181093_Sorghum_microbiome_072115-40_1_header.txt
+cat 2687453597_Sorghum_microbiome_072115-103_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2687453597_Sorghum_microbiome_072115-103_1_header.txt
+cat 2687453597_Sorghum_microbiome_072115-104_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2687453597_Sorghum_microbiome_072115-104_1_header.txt
+cat 2687453597_Sorghum_microbiome_072115-113_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2687453597_Sorghum_microbiome_072115-113_1_header.txt
+cat 2687453597_Sorghum_microbiome_072115-129_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2687453597_Sorghum_microbiome_072115-129_1_header.txt
+cat 2687453597_Sorghum_microbiome_072115-187_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2687453597_Sorghum_microbiome_072115-187_1_header.txt
+cat 2687453597_Sorghum_microbiome_072115-40_1.sam | grep -n "YI:f:" | awk -F 'YI:f:|\t' '{print $1"\t"$3"\t"$8"\t"$NF}' | sed 's/:HISEQ/|HISEQ/' | cut -d "|" -f 2 > 2687453597_Sorghum_microbiome_072115-40_1_header.txt
